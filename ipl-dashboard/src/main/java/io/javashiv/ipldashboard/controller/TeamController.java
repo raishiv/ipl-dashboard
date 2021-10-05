ed86049c51465ae014cb92edb2ed4ed72a2872b3
Shiv <raishiv@gmail.com>
2021-10-04 18:48:13 -0700

    Add backed Match API

--- ipl-dashboard/src/main/java/io/javashiv/ipldashboard/controller/TeamController.java
+++ ipl-dashboard/src/main/java/io/javashiv/ipldashboard/controller/TeamController.java
@@ -1,11 +1,16 @@
 package io.javashiv.ipldashboard.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.javashiv.ipldashboard.Model.Team;
+import io.javashiv.ipldashboard.Model.Match;
 import io.javashiv.ipldashboard.repository.MatchRepository;
 import io.javashiv.ipldashboard.repository.TeamRepository;
 
@@ -26,6 +31,13 @@ public class TeamController {
         Team team = this.teamRepository.findByTeamName(teamName);
         team.setMatches(matchRepository.FindLatestMatchesByteam(teamName, 4));
         return team;
+    }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year) {
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year + 1, 1,1);
+        return this.matchRepository.getMatchesByTeamBetweenDates(teamName, startDate, endDate);
     }
 
 }
